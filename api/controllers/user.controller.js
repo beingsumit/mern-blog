54fe7a232401fe9952ba86af1cb46420e4b9d81b
beingsumit <76860762+beingsumit@users.noreply.github.com>
2024-04-01 21:22:51 +0530

    created a test api route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,4 @@
+// in controller we write only logic  
+export const test = (req, res)=>{
+    res.json({message: 'API is working'});
+}
--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js'
+
 
 // we used "npm i dotenv" for importing mongodb connection 
 dotenv.config();
@@ -19,3 +21,6 @@ const app = express()
 app.listen(3000, ()=>{
     console.log("server is running on 3000");
 })
+
+// here we write api in userRouter file and call here 
+app.use('/api/user', userRouter);
